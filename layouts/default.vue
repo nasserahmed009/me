731375b587278a987c260eb7c01ed9dad42d5a88
nasser <anasserabdrabo@gmail.com>
2020-05-12 02:49:11 +0200

    adding dark theme toggling logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,44 @@
 <template>
-  <div>
+  <div :class="{ darkTheme: darkTheme, lightTheme: !darkTheme }">
+    <div class="toggleTheme" @click="darkTheme = !darkTheme">
+      <font-awesome-icon :icon="['fas', 'moon']" v-show="!darkTheme" />
+      <font-awesome-icon
+        :icon="['fas', 'sun']"
+        v-show="darkTheme"
+        style="color: #f1c40f"
+      />
+    </div>
     <nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
+<style scoped>
+.toggleTheme {
   display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+  position: fixed;
+  z-index: 1;
+  right: 0px;
+  top: 30%;
+  cursor: pointer;
+  border-radius: 20px 0px 0px 20px;
+  padding: 20px;
+  box-shadow: 0px 0px 3px 0px var(--shadowColor);
+  font-size: 20px;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.toggleTheme:hover {
+  padding-right: 30px;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.darkTheme {
+  background: var(--bg);
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      darkTheme: false
+    };
+  }
+};
+</script>
