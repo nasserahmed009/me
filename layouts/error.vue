77aeb8f71610400a463e6d542227650628468144
nasser <anasserabdrabo@gmail.com>
2020-05-13 03:12:40 +0200

    adding error 404

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="error">
+    <div class="container text-center" v-if="error.statusCode === 404">
+      <font-awesome-icon
+        :icon="['fas', 'ghost']"
+        style="color: #3498db; font-size: 100px"
+      />
+      <h1 class="textColor1">Are you lost ?</h1>
+      <p class="textColor2">Sorry, we couldn't find this page</p>
+      <a href="/" class="buttonMain">Back to the home page</a>
+    </div>
+
+    <div class="container text-center" v-else>
+      <font-awesome-icon
+        :icon="['fas', 'bomb']"
+        style="color: #3498db; font-size: 100px"
+      />
+      <h1 class="textColor1">It appears like there's an error ?</h1>
+      <p class="textColor2">
+        Sorry for the inconvenience, Please try again late
+      </p>
+      <a href="/" class="buttonMain">Back to the home page</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "blog" // you can set a custom layout for the error page
+};
+</script>
+
+<style scoped>
+.error {
+  min-height: calc(100vh - 100px - 95px);
+  background: var(--bg1);
+  padding: 100px 25px;
+}
+</style>
